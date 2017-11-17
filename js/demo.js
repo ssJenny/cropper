b062c76d617e8ca983eeeb67b5e07b933c062c43
shixi <sissi_sx@163.com>
2017-11-17 17:38:14 +0800

    .

--- js/demo.js
+++ js/demo.js
@@ -1,17 +1,39 @@
 window.onload = function () {
   var Cropper = window.Cropper;
   var uploadImg = document.getElementById('inputImage');
-  var imgContainer = document.querySelector('.img-container');
-  var image = imgContainer.getElementsByTagName('img').item(0);
-  var confirmBtn = document.getElementById('confirmBtn');
+  var cutContainer = document.getElementById('cutImg');
+  var image = cutContainer.getElementsByTagName('img').item(0);
+  var download = document.getElementById('confirmBtn');
   var URL = window.URL || window.webkitURL;
 
+  var serverURL = '';
+
+  var dataX;
+  var dataY;
+  var dataWidth;
+  var dataHeight;
+
+
   var options = {
-    // aspectRatio: 16 /9 ,
+    aspectRatio: 16 /9 ,
+    preview: '.img-preview',
+    crop: function (e) {
+      var data = e.detail;
+
+      console.log(data);
+      dataX = Math.round(data.x);
+      dataY = Math.round(data.y);
+      dataHeight = Math.round(data.height);
+      dataWidth = Math.round(data.width);
+
+    },
+    zoom: function (e) {
+      console.log(e.type, e.detail.ratio);
+    },
+
   };
 
   var cropper = new Cropper(image, options);
-  var originalImageURL = image.src;
   var uploadedImageType = 'image/jpeg';
   var uploadedImageURL;
 
@@ -34,7 +56,7 @@ window.onload = function () {
           image.src = uploadedImageURL = URL.createObjectURL(file);
           cropper.destroy();
           cropper = new Cropper(image, options);
-          inputImage.value = null;
+          uploadImg.value = null;
         } else {
           window.alert('Please choose an image file.');
         }
@@ -45,9 +67,34 @@ window.onload = function () {
     uploadImg.parentNode.className += ' disabled';
   }
 
-  confirmBtn.onclick = function () {
-    console.log(cropper)
-  }
 
+
+  download.onclick = function (e) {
+    var data = {
+      method: 'getCroppedCanvas',
+      option: {
+        width: dataWidth,
+        height: dataHeight
+      },
+
+    };
+    var result = cropper[data.method](data.option);
+    var newUrl = result.toDataURL(uploadedImageType);
+    console.log(newUrl);
+
+    $.ajax({
+      type: 'post',
+      async: true,
+      url: serverURL,
+      dataType: 'json',
+      data: {img: newUrl},
+      success: function (res) {
+        console.log(res)
+      },
+      error: function (xhr, ts, et) {
+        console.log(ts,et)
+      }
+    })
+  }
 
 };
